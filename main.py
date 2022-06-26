5295eea8ea46819076ecb14191fbc2adb3ccd177
BugaevAV <iustusvir@gmail.com>
2022-06-26 12:29:43 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from pprint import pprint
+import requests
+
+
+
+class YandexDisk:
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def _get_upload_link(self):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": "pythonProjectrequests\\test.txt", "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
+
+    def upload_file_to_disk(self, filename):
+        href = self._get_upload_link().get("href", "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+
+
+if __name__ == '__main__':
+    ya = YandexDisk(token='AQAAAABTRlo_AADLW2LeYlBvrEeqgvh9nzoWayQ')
+    ya.upload_file_to_disk(filename='test.txt')
